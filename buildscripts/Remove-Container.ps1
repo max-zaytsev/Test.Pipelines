f4161b63c12a307e00aa3fad16d41c1dddb2a6e5
Florian Winkler <fwi@comsol.ag>
2022-03-30 16:43:06 +0200

    Remove Container

---
+++ buildscripts/Remove-Container.ps1
@@ -0,0 +1,17 @@
+﻿Param(
+    # [ValidateSet('AzureDevOps','Local','AzureVM')]
+    # [string] $buildEnv = "AzureDevOps",
+
+    [Parameter(Mandatory=$false)]
+    [string] $containerName = $ENV:CONTAINERNAME,
+
+    [switch] $addBuildID
+)
+
+if ($addBuildID) {
+    $containerName = -join ($containerName, $($env:Build_BuildId))
+}
+
+Write-Host "Removing Container $containerName"
+
+Remove-BCContainer -containerName $containerName
